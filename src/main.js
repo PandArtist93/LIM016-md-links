ad8ab33e626e7c40a4278587bce85db190ff061c
Marielena <maizaga01@gmail.com>
2022-01-30 20:32:19 -0500

    revisión de funcionalidades

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 const { resolve } = require('path');
+const chalk = require('chalk');
 const fs = require('fs');
+const process = require('process');
 const { searchAllFiles, filterMdFiles, readAllFileMd } = require('../src/searchFiles.js');
-const { linksValidated, linkStats, statsAndValidateLinks } = require('../src/options.js');
+const { processOptions } = require('../src/options.js');
 
 const main = (path, options) => {
-    const completePath = absolutePath(path);    
+    const completePath = absolutePath(path);   
     processFiles(completePath, options);    
 }
 
 const absolutePath = (inputPath) => {
     if (inputPath == undefined) {
-        console.log("Debes ingresar una ruta valida y opciones válidas");
+        console.log(chalk.bold.red('Debes ingresar una ruta valida y opciones válidas'));
         process.exit(1);
     }
     const completePath = resolve(inputPath);
     
     if(!fs.existsSync(completePath)){
-        console.log("El path solicitado no existe");
+        console.log(chalk.bold.red("El path solicitado no existe"));
         process.exit(1);
     }
     return completePath
@@ -26,21 +28,7 @@ const processFiles = (completePath, options) => {
     const pathFiles = searchAllFiles(completePath);  // nos retorna un arreglo con los path
     const pathFilesMD = filterMdFiles(pathFiles); // nos retorna un arreglo con paths que tienen como ext (.md)
     const promisesFiles = readAllFileMd(pathFilesMD);  // lee y extrae los links contenido en cada uno de los archivos para retornarlos en un arreglo 
-    //processOptions(completePath, options);  
-    if (options.validate == true && options.stats == false){
-        linksValidated(promisesFiles);      
-    }
-    else if (options.validate == false && options.stats == true){
-        linkStats(promisesFiles);              
-    }
-    else if (options.validate == true && options.stats == true){
-        statsAndValidateLinks(promisesFiles);           
-    }
-    else{
-        promisesFiles.then( data => {
-            console.log(data.flat());
-        })
-    }
+    processOptions(promisesFiles, options);  
 }
 
 module.exports.main = main;
--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,20 @@
 const { argv } = require('process');
+const process = require('process');
+//const { console } = require('console');
 
 // Helper functions 
 
 const setOptions = (option, options) => {
-    const validOptions = ['v', 'validate', 's', 'stats'];
-    
     if (option) {
         const pseudoOption = option.replace(/-/g,'');
-        const result = validOptions.includes(pseudoOption)
-        if (pseudoOption == 'v' || pseudoOption == 'validate') {
+        if (pseudoOption == 'v' || pseudoOption == 'V' || pseudoOption == 'validate') {
             options['validate'] = true
         } 
-        else if (pseudoOption == 's' || pseudoOption == 'stats') {
+        else if (pseudoOption == 's' || pseudoOption == 'S' || pseudoOption == 'stats') {
             options['stats'] = true
         }       
         else {
-            console.log('opción invalida');
+            console.log('invalid Options');
             process.exit(1);
         }
     }
@@ -31,7 +30,7 @@ const parseOptionalArguments = () => {
     const option1 = argv[3];
     const option2 = argv[4];
    
-    options = setOptions(option1, options);    
+    options = setOptions(option1, options);
     options = setOptions(option2, options);
     //console.log(options);
     return options
